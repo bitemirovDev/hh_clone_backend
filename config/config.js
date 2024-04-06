f853c72720678498f9b805074974a0cdfa22ae93
Tair Bitemirov <beyself@vk.com>
2024-04-07 02:33:15 +0500

    Allow adding secret information

--- config/config.js
+++ config/config.js
@@ -1,26 +1,28 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
+
+productionPass = "AVNS_-hVgf6yzQEBD84dN6Lu";
 
 module.exports = {
-    development: {
-        username: 'admin',
-        password: 'root',
-        database: 'admin',
-        host: 'localhost',
-        dialect: 'postgres', 
+  development: {
+    username: "admin",
+    password: "root",
+    database: "admin",
+    host: "localhost",
+    dialect: "postgres",
+  },
+  production: {
+    username: "doadmin",
+    password: productionPass,
+    database: "defaultdb",
+    host: "db-postgresql-fra1-60574-do-user-14613666-0.b.db.ondigitalocean.com",
+    dialect: "postgres",
+    port: 25060,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(path.resolve("config", "ca-certificate.crt")),
+      },
     },
-    production: {
-        username: 'doadmin',
-        password: 'AVNS_-hVgf6yzQEBD84dN6Lu',
-        database: 'defaultdb',
-        host: 'db-postgresql-fra1-60574-do-user-14613666-0.b.db.ondigitalocean.com',
-        dialect: 'postgres',
-        port: 25060,
-        dialectOptions: {
-            ssl:{
-                ca: fs.readFileSync(path.resolve("config", "ca-certificate.crt"))
-            }
-        }
-    },
-    secretKey: 'секретный_ключ'
+  },
+  secretKey: "секретный_ключ",
 };
